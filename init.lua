7e7e12b88c4a02c19f7366415ee8bc67f67ffe1f
fodesu <himeno.senaa@gmail.com>
2023-09-23 15:06:55 +0800

    neovim for lazynvim, onedark was installed

---
+++ init.lua
@@ -0,0 +1,24 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+
+
+require("lazy").setup({ import = "plugins"})
+require('onedark').load()
